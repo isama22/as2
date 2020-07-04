97eae78d57eb0737b38521927cb55c6618e8e79b
isama22 <elisabethmartinez@Elisabeths-MacBook-Pro.local>
2020-07-04 03:58:15 -0700

    figurative photo and page working

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,13 +7,21 @@ urlpatterns = [
   path('contact', views.contact, name="contact"),
 
   path('accounts/signup/', views.signup, name='signup'),
+
   path('digitals/', views.digitals_index, name="digitals_index"),
   path('digitals/<int:digital_id>/', views.digitals_detail, name='digitals_detail'),
   path('digitals/create/', views.DigitalsCreate.as_view(), name='digitals_create'),
   path('digitals/<int:pk>/update/', views.DigitalUpdate.as_view(), name='digitals_update'),
   path('digitals/<int:pk>/delete/', views.DigitalDelete.as_view(), name='digitals_delete'),
-
   path('digitals/<int:digital_id>/digital_photo/', views.digital_photo, name='digital_photo'),
   path('digitals/<int:digital_id>/delete_digital_photo/', views.delete_digital_photo, name='delete_digital_photo'),
+
+  path('figuratives/', views.figuratives_index, name="figuratives_index"),
+  path('figuratives/<int:figurative_id>/', views.figuratives_detail, name='figuratives_detail'),
+  path('figuratives/create/', views.FigurativesCreate.as_view(), name='figuratives_create'),
+  path('figuratives/<int:pk>/update/', views.FigurativeUpdate.as_view(), name='figuratives_update'),
+  path('figuratives/<int:pk>/delete/', views.FigurativeDelete.as_view(), name='figuratives_delete'),
+  path('figuratives/<int:figurative_id>/figurative_photo/', views.figurative_photo, name='figurative_photo'),
+  path('figuratives/<int:figurative_id>/delete_figurative_photo/', views.delete_figurative_photo, name='delete_figurative_photo'),
   
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -70,7 +70,57 @@ def delete_digital_photo(request, digital_id):
     
     return redirect('digitals_detail', digital_id=digital_id)    
 
+def figuratives_index(request):
+  figuratives = Figurative.objects.all()
+  return render(request, 'figuratives/index.html', { 'figuratives': figuratives })
+
+def figuratives_detail(request, figurative_id):
+  figurative = Figurative.objects.get(id=figurative_id)
+  return render(request, 'figuratives/detail.html', { 'figurative': figurative }) 
+
+class FigurativesCreate(LoginRequiredMixin, CreateView):
+  model = Figurative
+  fields = '__all__'
+  success_url = '/figuratives/'
+
+class FigurativeUpdate(LoginRequiredMixin, UpdateView):
+  model = Figurative
+  # Let's disallow the renaming of a cat by excluding the name field!
+  fields = '__all__'
+  success_url = '/figuratives/'
+
+class FigurativeDelete(LoginRequiredMixin, DeleteView):
+  model = Figurative
+  success_url = '/figuratives/'
+  
+def figurative_photo(request, figurative_id):
+    # photo-file will be the "name" attribute on the <input type="file">
+    photo_file = request.FILES.get('photo-file', None)
+    if photo_file:
+        s3 = boto3.client('s3')
+        # need a unique "key" for S3 / needs image file extension too
+        key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+        # just in case something goes wrong
+        try:
+            s3.upload_fileobj(photo_file, BUCKET, key)
+            # build the full url string
+            url = f"{S3_BASE_URL}{BUCKET}/{key}"
+            # we can assign to cat_id or cat (if you have a cat object)
+            photo =FigurativePhoto(url=url, figurative_id=figurative_id)
+            photo.save()
+        except:
+            print('An error occurred uploading file to S3')
+    return redirect('figuratives_detail', figurative_id=figurative_id)  
+
+
+def delete_figurative_photo(request, figurative_id):
+    #.all() deletes all photos if two images were uploaded on accident while loading, but deletes ALL fig photos
+    key = FigurativePhoto.objects.all()
+    key.delete()
     
+    return redirect('figuratives_detail', figurative_id=figurative_id)
+
+
 def signup(request):
   error_message = ''
   if request.method == 'POST':
