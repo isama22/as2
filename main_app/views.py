6d0fdf4739c6e8d6118d8b9fb75b51c8bd532bce
isama22 <elisabethmartinez@Elisabeths-MacBook-Pro.local>
2020-07-04 03:26:30 -0700

    auth done

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Fiber, Figurative, Digital, FiberPhoto, FigurativePhoto, DigitalPhoto
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 # Add the following import
 from django.http import HttpResponse
 
@@ -13,3 +15,21 @@ def about(request):
 def contact(request):
   return render(request, 'contact.html')  
 
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    # This is how to create a 'user' form object
+    # that includes the data from the browser
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      # This will add the user to the database
+      user = form.save()
+      # This is how we log a user in via code
+      login(request, user)
+      return redirect('/')
+    else:
+      error_message = 'Invalid sign up - try again'
+  # A bad POST or a GET request, so render signup.html with an empty form
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
